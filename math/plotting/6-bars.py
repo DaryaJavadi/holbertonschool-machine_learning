b3f9ccb540d58e5dbea2fea36f57cfcc3c9e0e96
DaryaJavadi <darya.javadi15@gmail.com>
2026-01-11 17:47:46 +0400

    bars

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -1,8 +1,29 @@
 #!/usr/bin/env python3
+"""
+6-bars.py
+This module contains a function `bars` that plots a stacked bar graph
+showing the quantity of different fruits (apples, bananas, oranges, peaches)
+for three people (Farrah, Fred, Felicia).
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def bars():
+    """
+    Plot a stacked bar graph of fruit quantities per person.
+
+    - Apples are red
+    - Bananas are yellow
+    - Oranges are orange (#ff8000)
+    - Peaches are peach (#ffe5b4)
+    - Bars are stacked in order: apples, bananas, oranges, peaches
+    - Bars width: 0.5
+    - Y-axis label: 'Quantity of Fruit'
+    - Y-axis range: 0 to 80 with ticks every 10 units
+    - Title: 'Number of Fruit per Person'
+    """
     np.random.seed(5)
     fruit = np.random.randint(0, 20, (4, 3))
 
