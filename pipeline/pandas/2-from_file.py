db0dcd4d55e2af459e8bc6c4805ccf3e26ca989c
DaryaJavadi <darya.javadi15@gmail.com>
2026-01-11 14:48:29 +0400

    load data

---
+++ pipeline/pandas/2-from_file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+This module contains a function `from_file` that loads data
+from a file into a pandas DataFrame using a specified delimiter.
+"""
+
+import pandas as pd
+
+
+def from_file(filename, delimiter):
+    """
+    Load data from a file as a pandas DataFrame.
+
+    Parameters:
+    filename (str): The path to the file to load.
+    delimiter (str): The column separator used in the file.
+
+    Returns:
+    pd.DataFrame: The loaded DataFrame.
+    """
+    return pd.read_csv(filename, delimiter=delimiter)
