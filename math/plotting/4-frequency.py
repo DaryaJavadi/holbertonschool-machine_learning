c194d8cd8e3faddd12e159639eceaeafb92a58c3
DaryaJavadi <darya.javadi15@gmail.com>
2026-01-11 17:30:50 +0400

    frequency

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python3
+"""
+4-frequency.py
+This module contains a function `frequency` that plots a histogram
+of student grades for Project A with exact binning and styling.
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.random.seed(5)
-student_grades = np.random.normal(68, 15, 50)
 
-bins = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+def frequency():
+    """
+    Plot a histogram of student grades:
+    - Bins every 10 units
+    - Bars outlined in black
+    - Labels and title set
+    """
+    np.random.seed(5)
+    student_grades = np.round(np.random.normal(68, 15, 50))
 
-plt.xlabel('Grades')
-plt.ylim(0, 30)
-plt.xlim(0, 100)
-plt.ylabel('Number of Students')
-plt.title('Project A')
-plt.hist(student_grades, bins, edgecolor='black')
-plt.xticks(np.arange(0, 110, 10))
-plt.show()
+    plt.figure(figsize=(6.4, 4.8))
+    bins = np.arange(0, 101, 10)
+    plt.hist(student_grades, bins=bins, edgecolor='black')
+
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+    plt.title("Project A")
+    plt.show()
