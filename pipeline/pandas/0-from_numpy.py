42625cf8fb4644be4631da9f9c8900c181673291
DaryaJavadi <darya.javadi15@gmail.com>
2026-01-11 14:43:10 +0400

    numpy 0

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+
+"""
+This module contains a function `from_numpy` that converts
+a NumPy ndarray into a pandas DataFrame with alphabetically
+capitalized column names.
+"""
+
 import pandas as pd
 
 
