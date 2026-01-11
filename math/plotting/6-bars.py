eff795c1faa6704bfe4461a72e825633c186aad9
DaryaJavadi <darya.javadi15@gmail.com>
2026-01-11 17:44:56 +0400

    bars

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -2,29 +2,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def bars():
-    np.random.seed(5)
-    fruit = np.random.randint(0, 20, (4, 3))
-    plt.figure(figsize=(6.4, 4.8))
+np.random.seed(5)
+fruit = np.random.randint(0, 20, (4, 3))
 
-    colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
-    labels = ['Apples', 'Bananas', 'Oranges', 'Peaches']
+# your code here
 
-    x = np.arange(fruit.shape[1])
+rows = ('apples', 'bananas', 'oranges', 'peaches')
+columns = ('Farrah', 'Fred', 'Felicia')
+index = columns
+colors = ('red', 'yellow', '#ff8000', '#ffe5b4')
+n_rows = len(fruit)
+bar_width = 0.5
+y_offset = np.zeros(len(columns))
 
-    bottom = np.zeros(fruit.shape[1])
 
-    for i in range(fruit.shape[0]):
-        plt.bar(x, fruit[i], bottom=bottom, color=colors[i], width=0.5, label=labels[i])
-        bottom += fruit[i]
+for row in range(n_rows):
+    plt.bar(index, fruit[row], bar_width, bottom=y_offset,
+            color=colors[row], label=rows[row])
+    y_offset = y_offset + fruit[row]
 
-    plt.xticks(x, ['Farrah', 'Fred', 'Felicia'])
 
-    plt.ylabel("Quantity of Fruit")
-    plt.ylim(0, 80)
-    plt.yticks(np.arange(0, 81, 10))
-
-    plt.title("Number of Fruit per Person")
-    plt.legend()
-
-    plt.show()
+plt.legend()
+plt.yticks(np.arange(0, 90, 10))
+plt.ylabel('Quantity of Fruit')
+plt.title("Number of Fruit per Person")
+plt.show()
