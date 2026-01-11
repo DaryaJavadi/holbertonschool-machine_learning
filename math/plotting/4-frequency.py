1d8f07b25f29c2d1a5f8294870664c5a99933d4e
DaryaJavadi <darya.javadi15@gmail.com>
2026-01-11 17:28:07 +0400

    frequency

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -17,11 +17,10 @@ def frequency():
     - Labels and title set
     """
     np.random.seed(5)
-    student_grades = np.random.normal(68, 15, 50).astype(int)
+    student_grades = np.round(np.random.normal(68, 15, 50))
+    
     plt.figure(figsize=(6.4, 4.8))
-
     bins = np.arange(0, 101, 10)
-
     plt.hist(student_grades, bins=bins, edgecolor='black')
 
     plt.xlabel("Grades")
