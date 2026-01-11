b2f281e8732bd4e30946edfa6bfcb8c406d34373
DaryaJavadi <darya.javadi15@gmail.com>
2026-01-11 17:14:07 +0400

    plotting

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
+"""
+0-line.py
+This module contains a function `line` that plots a cubic line graph
+from 0 to 10 using a solid red line.
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
+    """
+    Plot a cubic function from 0 to 10 as a solid red line.
+    The x-axis is explicitly set from 0 to 10.
+    """
     x = np.arange(0, 11)
     y = x ** 3
 
     plt.figure(figsize=(6.4, 4.8))
-
     plt.plot(x, y, color='red', linestyle='-')
-
     plt.xlim(0, 10)
-
     plt.show()
