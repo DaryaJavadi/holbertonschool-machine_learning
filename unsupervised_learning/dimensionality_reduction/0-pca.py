b318a6655f30260d13bdffeeac12681280d07ed8
DaryaJavadi <darya.javadi15@gmail.com>
2026-05-17 18:13:31 +0400

    dimensionality reduction

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -1,28 +1,13 @@
 #!/usr/bin/env python3
-"""PCA function using numpy SVD"""
+"""A module that does the trick"""
 import numpy as np
 
 
 def pca(X, var=0.95):
-    """
-    Function that performs PCA on a dataset.
-
-    Args:
-        X: numpy.ndarray of shape (n, d) where:
-           n is the number of data points
-           d is the number of dimensions in each point
-           all dimensions have a mean of 0 across all data points
-        var: fraction of the variance that the PCA transformation
-             should maintain
-
-    Returns:
-        W: weights matrix of shape (d, nd) where nd is the
-           new dimensionality of the transformed X
-    """
-    _, s, vh = np.linalg.svd(X)
-
-    variance = np.cumsum(s) / np.sum(s)
-
-    r = np.argwhere(variance >= var)[0, 0]
-
-    return vh[:r + 1].T
+    """A function that does the trick"""
+    u, Sigma, vh = np.linalg.svd(X, full_matrices=False)
+    cumulative_var = np.cumsum(Sigma) / np.sum(Sigma)
+    r = (np.argwhere(cumulative_var >= var))[0, 0]
+    w = vh.T
+    wr = w[:, :r + 1]
+    return wr
--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -1,26 +1,13 @@
 #!/usr/bin/env python3
-"""PCA on a dataset"""
+"""A module that does the trick"""
 import numpy as np
 
 
 def pca(X, ndim):
-    """
-    Performs PCA on a dataset.
-
-    Args:
-        X: numpy.ndarray of shape (n, d)
-            n is the number of data points
-            d is the number of dimensions in each point
-        ndim: new dimensionality of the transformed X
-
-    Returns:
-        T: numpy.ndarray of shape (n, ndim) containing
-           the transformed version of X
-    """
-    X_m = X - np.mean(X, axis=0)
-
-    _, _, vh = np.linalg.svd(X_m)
-
-    W = vh[:ndim].T
-
-    return np.matmul(X_m, W)
+    """A function that does the trick"""
+    X_mean = X - np.mean(X, axis=0)
+    U, S, V = np.linalg.svd(X_mean)
+    W = V.T
+    Wr = W[:, :ndim]
+    T = np.dot(X_mean, Wr)
+    return T
