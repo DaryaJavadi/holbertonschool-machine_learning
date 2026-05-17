b3f099ea77b5386d23272967e51c1b2d7ab6e6e0
DaryaJavadi <darya.javadi15@gmail.com>
2026-05-17 18:11:48 +0400

    dimensionality reduction

---
+++ unsupervised_learning/dimensionality_reduction/4-P_affinities.py
@@ -0,0 +1,37 @@
+!/usr/bin/env python3
+"""
+module for task 4
+"""
+
+import numpy as np
+P_init = __import__('2-P_init').P_init
+HP = __import__('3-entropy').HP
+
+
+def P_affinities(X, tol=1e-5, perplexity=30.0):
+    """
+    calculates symmetric P affinities of data set
+    """
+    D, P, betas, H = P_init(X, perplexity)
+    for i in range(X.shape[0]):
+        Hi, Pi = HP(np.append(D[i, :i], D[i, i+1:]), betas[i])
+        mini = None
+        maxi = None
+        Hdiff = Hi - H
+        while np.abs(Hdiff) > tol:
+            if Hdiff > 0:
+                mini = betas[i].copy()
+                if maxi is None:
+                    betas[i] = betas[i] * 2
+                else:
+                    betas[i] = (betas[i] + maxi) / 2
+            else:
+                maxi = betas[i].copy()
+                if mini is None:
+                    betas[i] = betas[i] / 2
+                else:
+                    betas[i] = (betas[i] + mini) / 2
+            Hi, Pi = HP(np.append(D[i, :i], D[i, i + 1:]), betas[i])
+            Hdiff = Hi - H
+        P[i, np.concatenate((np.r_[0:i], np.r_[i+1:X.shape[0]]))] = Pi
+    return (P.T + P) / (2 * X.shape[0])
---
+++ unsupervised_learning/dimensionality_reduction/README.md
@@ -0,0 +1,21 @@
+# Dimensionality Reduction
+
+This project explores dimensionality reduction techniques such as PCA
+(Principal Component Analysis) and t-SNE (t-distributed Stochastic
+Neighbor Embedding).
+
+## What is Dimensionality Reduction?
+Dimensionality reduction is the process of reducing the number of features
+in a dataset while preserving as much information as possible. It is used
+to simplify models, reduce computation time, and help visualize
+high-dimensional data.
+
+## Techniques Covered
+
+### PCA (Linear)
+PCA finds the directions of maximum variance in the data and projects
+it onto a lower-dimensional space using Singular Value Decomposition (SVD).
+
+### t-SNE (Non-linear)
+t-SNE is used mainly for visualization. It maps high-dimensional data
+to 2D or 3D by preserving local structure using probability distributions.
